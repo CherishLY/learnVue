178e0ad2a4f16a869eaa12ecb41e6d9b1fdf535d
CherishLY <1605457871@qq.com>
2017-09-11 11:25:23 +0800

    add vue-for example

---
+++ js/vue-for.js
@@ -0,0 +1,23 @@
+/**
+ * Created by lilei on 2017/9/11.
+ */
+var app=new Vue({
+    el:"#app",
+    data:{
+        students:["李磊","韩梅梅","ABC"],
+        grade:[
+            {
+                grade:"一年级",
+                name:"李磊"
+            },
+            {
+                grade:"二年级",
+                name:"韩梅梅"
+            },
+            {
+                grade:"一年级",
+                name:"ABC"
+            },
+        ]
+    }
+})
