08610508fe59ca6038f3becc34d4037a1b95eb60
CherishLY <1605457871@qq.com>
2017-09-12 13:35:05 +0800

    add 平行组件的通信

--- js/vue-emit.js
+++ js/vue-emit.js
@@ -1,3 +1,38 @@
 /**
  * Created by lilei on 2017/9/12.
  */
+
+var Event=new Vue();
+
+Vue.component('huahua', {
+    template: '<div>我说：<input @keyup="huahua_say" v-model="i_said" type="text">{{ i_said }}</div>',
+    data: function () {
+        return {
+            i_said: ""
+        }
+    },
+    methods: {
+        huahua_say: function () {
+            Event.$emit("huahua-said-something",this.i_said);
+        }
+    }
+})
+Vue.component('meimei', {
+    template: '<div>花花说：{{huahua_said}}</div>',
+    data: function () {
+        return {
+            huahua_said: ""
+        }
+    },
+    mounted:function(){
+        var me=this;
+        Event.$on("huahua-said-something",function(data){
+            console.log(data);
+            me.huahua_said=data;
+        })
+    }
+})
+
+new Vue({
+    el:'#app'
+})
