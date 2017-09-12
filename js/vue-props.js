5d8b9046aa0766c03c22d6e695a575146fdf846a
CherishLY <1605457871@qq.com>
2017-09-12 11:15:08 +0800

    父子组件通信

--- js/vue-props.js
+++ js/vue-props.js
@@ -10,6 +10,32 @@ Vue.component('art',{
         }
     }
 })
+
+
+Vue.component('newsbutton',{
+    template:'<div> <showid @show_news="show_id"></showid> </div>',
+    data:function(){
+        return {
+            newsid:null
+        }
+    },
+    methods:{
+        show_id:function(data){
+            this.newsid=data.artid;
+            alert(this.newsid);
+        }
+    }
+})
+
+Vue.component('showid',{
+    template:'<button @click="show_id()" artid="123456" >查询文章id</button>',
+    methods:{
+        show_id:function(e){
+           this.$emit("show_news",{artid:123456})
+        }
+    }
+})
+
 new Vue({
     el:"#app",
 })
